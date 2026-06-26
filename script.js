fa018cf2b77ccae866aa46ffa5f13f646edf2af6
victorhernandezvqz <victorhernandezvqz@gmail.com>
2026-06-26 15:08:12 -0600

    Efecto menu

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+
+// ==========================
+// NAVBAR TRANSPARENTE AL SCROLL
+// ==========================
+
+const header = document.querySelector(".header");
+
+
+window.addEventListener("scroll", () => {
+
+  if (window.scrollY > 50) {
+
+    header.classList.add("header--scroll");
+
+  } else {
+
+    header.classList.remove("header--scroll");
+
+  }
+
+});
